05ff4acde239b9e1a8001a8794b92eecd3f9d94a
Santino Degra <santinodegra73@gmail.com>
2024-03-11 17:45:51 -0300

    Update and fix selector from editor

--- src/components/quill-editor/Selector.tsx
+++ src/components/quill-editor/Selector.tsx
@@ -3,33 +3,45 @@
 import { useState } from "react";
 import { Select, SelectContent, SelectItem, SelectTrigger } from "../ui/select";
 import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
-import { ChevronDown } from "lucide-react";
+import { ChevronDown, Paintbrush, Palette } from "lucide-react";
+import { cn } from "@/lib/utils";
 
 interface SelectorDropdownProps {
   defaultValue: any;
   items: any[];
+  classname?: string;
+  quill: any;
 }
 
 const SelectorDropdown: React.FC<SelectorDropdownProps> = ({
   defaultValue,
   items,
+  classname,
+  quill
 }) => {
   const [selectedOption, setSelectedOption] = useState(defaultValue);
 
   const handleSelect = (option: any) => {
+    console.log(`bg-[${option.value.hex}]`)
     setSelectedOption(option);
-    option.function();
+  
+    if (option.content === 'font' || option.content === 'align') {
+      return quill.format(option.content, option.value);
+    }
+
+    if (option.content === 'color' || option.content === 'background') {
+      return quill.format(option.content, option.value.hex);
+    }
   };
 
   return (
     <Popover>
       <PopoverTrigger className="!w-fit !flex items-center !shadow-none hover:!bg-white-2-sec rounded-sm">
-        <div className="flex items-center gap-1">
+        <div className={cn("flex items-center gap-1", classname)}>
           {selectedOption ? (
             <div className="flex gap-1 items-center">
-              {selectedOption.icon ? (
-                selectedOption.icon
-              ) : (
+              {selectedOption.content === "align" && <>{selectedOption.icon}</>}
+              {selectedOption.content === "font" && (
                 <p
                   className={`cursor-pointer select-none ${
                     selectedOption.value == "monospace"
@@ -37,8 +49,16 @@ const SelectorDropdown: React.FC<SelectorDropdownProps> = ({
                       : selectedOption.value == "serif" && "font-serif"
                   }`}
                 >
-                  {selectedOption.value}
+                  {selectedOption.value.length <= 0
+                    ? "Sans Serif"
+                    : selectedOption.value}
                 </p>
+              )}
+              {selectedOption.content === "color" && (
+                <Palette width={18} height={18} className="text-black" />
+              )}
+              {selectedOption.content === "background" && (
+                <Paintbrush width={18} height={18} className="text-black" />
               )}
               <ChevronDown width={14} height={14} className="text-grey" />
             </div>
@@ -48,17 +68,23 @@ const SelectorDropdown: React.FC<SelectorDropdownProps> = ({
         </div>
       </PopoverTrigger>
       <PopoverContent className="shadow-pop w-fit mt-1 p-1">
-        <div className="rounded-md flex flex-col">
+        <div
+          className={cn(
+            "rounded-md",
+            items[0].content === "color" || items[0].content === "background"
+              ? "grid grid-cols-3 gap-1"
+              : "flex flex-col"
+          )}
+        >
           {!!items.length &&
             items.map((option: any) => (
               <div
-                key={option.value}
+                key={option.value.content || option.value}
                 onClick={() => handleSelect(option)}
                 className="p-1 hover:bg-white-2-sec rounded-sm transition-colors"
               >
-                {option.icon ? (
-                  option.icon
-                ) : (
+                {option.content === "align" && <>{option.icon}</>}
+                {option.content === "font" && (
                   <p
                     className={`cursor-pointer select-none ${
                       option.value == "monospace"
@@ -66,8 +92,18 @@ const SelectorDropdown: React.FC<SelectorDropdownProps> = ({
                         : option.value == "serif" && "font-serif"
                     }`}
                   >
-                    {option.value}
+                    {option.value.length <= 0 ? "Sans Serif" : option.value}
                   </p>
+                )}
+                {option.content === "color" && (
+                  <div
+                    className={`w-4 h-4 rounded-full bg-[${option.value.hex}] border border-black/10 cursor-pointer`}
+                  ></div>
+                )}
+                {option.content === "background" && (
+                  <div
+                    className={`w-4 h-4 rounded-full bg-[${option.value.hex}] border border-outline cursor-pointer`}
+                  ></div>
                 )}
               </div>
             ))}
