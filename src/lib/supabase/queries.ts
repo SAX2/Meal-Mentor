27205cadfad0517f82984c9529c83c47846fcc13
Santino Degra <santinodegra73@gmail.com>
2024-03-13 12:01:24 -0300

    Updates database:
    -Install supbase CLI
    -Add new folder queries
    -Add table types from supabase

--- src/lib/supabase/queries.ts
+++ src/lib/supabase/queries.ts
@@ -1,16 +1,45 @@
 import db from "./db";
 import { and, eq } from "drizzle-orm";
 import { folders } from "../../../migrations/schema";
+import { Folder } from "./supabase.types";
 
-export const getFolders = async ({folderId, userId}: { folderId?: string, userId: string }) => {
+export const createFolder = async (folder: Folder) => {
   try {
-    if (folderId) {
-      const response = await db.select().from(folders).where(and(eq(folders.id, folderId), eq(folders.folderOwner, userId)));
-      return { data: response, error: null };
-    }
-    const response = await db.select().from(folders).where(eq(folders.folderOwner, userId));
+    await db.insert(folders).values(folder);
+    return { data: null, error: null };
+  } catch (error) {
+    console.log(error);
+    return { data: null, error: 'Error' };
+  }
+};
+
+export const deleteFolder = async (folderId: string) => {
+  if (!folderId) return;
+
+  try {
+    await db.delete(folders).where(eq(folders.id, folderId));
+    return { data: null, error: null };
+  } catch (error) {
+    console.log(error);
+    return { data: null, error: 'Error' };
+  }
+};
+
+export const getFolders = async (userId: string) => {
+  try {
+    const response = await db.select().from(folders).where(eq(folders.folderOwner, userId)) as Folder[];
     return { data: response, error: null };
 
+  } catch (error) {
+    console.log(error);
+    return { data: null, error: 'Error' };
+  }
+};
+
+export const getFolderDetails = async ({ folderId, userId }: { folderId: string, userId: string }) => {
+  try {
+    const response = await db.select().from(folders).where(and(eq(folders.id, folderId), eq(folders.folderOwner, userId))).limit(1) as Folder[];
+    return { data: response, error: null };
   } catch (error) {
     console.log(error);
     return { data: null, error: 'Error' };
